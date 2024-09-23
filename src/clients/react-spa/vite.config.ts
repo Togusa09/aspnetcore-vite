ff429f1699264d581bee66b6abd85cff862feca2
Ben Hockley <ben.hockley@purple.telstra.com>
2024-09-23 14:41:25 +1000

    Resetting SPA config to fix issue

--- src/clients/react-spa/vite.config.ts
+++ src/clients/react-spa/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite"
-import checker from "vite-plugin-checker";
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import checker from 'vite-plugin-checker';
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,16 +8,4 @@ export default defineConfig({
     react(),
     checker({ typescript: { tsconfigPath: "tsconfig.app.json" }}),
   ],
-  appType: 'custom',
-  //root: 'src',
-  publicDir: 'public',
-  build: {
-      manifest: true,
-      emptyOutDir: true,
-      outDir: '../../WebApp/wwwroot',
-      //assetsDir: '',
-      rollupOptions: {
-          input: 'src/main.tsx'
-      },
-  }
 })
