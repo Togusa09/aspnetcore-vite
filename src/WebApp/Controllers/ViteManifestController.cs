f3ab8d5d1b0b1fb8fef5ad6b04839a4d7421c2ba
Ben Hockley <ben.hockley@purple.telstra.com>
2024-09-24 12:09:35 +1000

    Updating SPA

---
+++ src/WebApp/Controllers/ViteManifestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vite.AspNetCore;
+
+namespace WebApp.Controllers
+{
+    public class ViteManifestController(IViteManifest manifest) : Controller
+    {
+        public IActionResult Index()
+        {
+            var files = manifest.Keys;
+
+            return View(files);
+        }
+    }
+}
---
+++ src/clients/react-spa/src/components/FetchData.tsx
@@ -0,0 +1,63 @@
+import { Component } from 'react';
+
+
+
+export class FetchData extends Component<object, {forecasts: [], loading: boolean}> {
+  //static displayName = FetchData.name;
+
+  constructor(props: object) {
+    super(props);
+    this.state = { forecasts: [], loading: true };
+  }
+
+  public componentDidMount() {
+    this.populateWeatherData();
+  }
+
+  public async populateWeatherData() {
+    const response = await fetch('weatherforecast');
+    const data = await response.json();
+    this.setState({ forecasts: data, loading: false });
+  }
+
+  static renderForecastsTable(forecasts: {
+    date: string,temperatureC: number, temperatureF: number, summary:string
+  }[]) {
+    return (
+      <table className="table table-striped" aria-labelledby="tableLabel">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Temp. (C)</th>
+            <th>Temp. (F)</th>
+            <th>Summary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {forecasts.map(forecast =>
+            <tr key={forecast.date}>
+              <td>{forecast.date}</td>
+              <td>{forecast.temperatureC}</td>
+              <td>{forecast.temperatureF}</td>
+              <td>{forecast.summary}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    );
+  }
+
+  render() {
+    const contents = this.state.loading
+      ? <p><em>Loading...</em></p>
+      : FetchData.renderForecastsTable(this.state.forecasts);
+
+    return (
+      <div>
+        <h1 id="tableLabel">Weather forecast</h1>
+        <p>This component demonstrates fetching data from the server.</p>
+        {contents}
+      </div>
+    );
+  }
+}
